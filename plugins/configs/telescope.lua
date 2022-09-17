85bf6b091f41c44c95cb61fbc3935d2c9f317eb8
derek_lee <20820343@student.curtin.edu.au>
2022-09-17 15:44:19 +0400

    Configured floating list of diagnostics with telescope

--- plugins/configs/telescope.lua
+++ plugins/configs/telescope.lua
@@ -1,8 +1,9 @@
 local actions = require('telescope.actions')
+-- local builtin = require('telescope.builtin')
 local present, telescope = pcall(require, "telescope")
 
 if not present then
-  return
+    return
 end
 
 vim.g.theme_switcher_loaded = true
@@ -10,28 +11,37 @@ vim.g.theme_switcher_loaded = true
 require("base46").load_highlight "telescope"
 
 local options = {
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-        ["<esc>"] = actions.close,
-        ["<CR>"] = actions.select_default + actions.center
-      },
-  },
-
-  extensions_list = { "themes", "terms" },
-},
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+                ["<esc>"] = actions.close,
+                ["<CR>"] = actions.select_default + actions.center
+            },
+        },
+        extensions_list = { "themes", "terms" },
+        -- builtin.diagnostics({
+        --     no_sign = true,
+        --     line_width = 200,
+        --     bufnr = 0,
+        -- })
+    },
 }
-
 -- check for any override
 -- options = require("core.utils").load_override(options, "nvim-telescope/telescope.nvim")
 telescope.setup(options)
 
 -- load extensions
 pcall(function()
-  for _, ext in ipairs(options.extensions_list) do
-    telescope.load_extension(ext)
-  end
+    for _, ext in ipairs(options.extensions_list) do
+        telescope.load_extension(ext)
+    end
 end)
+
+-- builtin.diagnostics({
+--     no_sign = true,
+--     line_width = 200,
+--     bufnr = 0,
+-- })

63d709b3c0349611a6eb36b567613f726d85cde3
derek_lee <20820343@student.curtin.edu.au>
2022-10-06 11:37:20 +0400

    Changed base46 to forked repo for adding new theme

--- plugins/init.lua
+++ plugins/init.lua
@@ -3,74 +3,93 @@
 local override = require "custom.override"
 
 return {
-----------------------------------------------------------------------------------------
-    -- Installing new plugins
-    ["p00f/nvim-ts-rainbow"] = {},
+  ----------------------------------------------------------------------------------------
+  -- Installing new plugins
+  ["p00f/nvim-ts-rainbow"] = {},
 
-    -- Custom lsp config
-    ["neovim/nvim-lspconfig"] = {
-        config = function()
-            require "plugins.configs.lspconfig"
-            require "custom.plugins.configs.lspconfig"
-        end,
-    },
+  -- Custom lsp config
+  ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.configs.lspconfig"
+    end,
+  },
 
-    -- Smart comments
-    ["tpope/vim-commentary"] = {},
+  -- Smart comments
+  ["tpope/vim-commentary"] = {},
 
-    -- Telescope config overwrite
-    ["nvim-telescope/telescope.nvim"] = {
-        config = function()
-            require "plugins.configs.telescope"
-            require "custom.plugins.configs.telescope"
-        end,
-    },
+  -- Telescope config overwrite
+  ["nvim-telescope/telescope.nvim"] = {
+    config = function()
+      require "plugins.configs.telescope"
+      require "custom.plugins.configs.telescope"
+    end,
+  },
 
-    -- Reconfigured blankline for context highlight indenting and rainbow indents
-    ["lukas-reineke/indent-blankline.nvim"] = {
-        config = function()
-            require "custom.plugins.configs.blankline"
-        end,
-    },
+  -- Reconfigured blankline for context highlight indenting and rainbow indents
+  ["lukas-reineke/indent-blankline.nvim"] = {
+    config = function()
+      require "custom.plugins.configs.blankline"
+    end,
+  },
 
-    -- Holds context of current line
-    ["nvim-treesitter/nvim-treesitter-context"] = {
-        config = function()
-            require "custom.plugins.configs.tscontext"
-        end,
-    },
+  -- Holds context of current line
+  ["nvim-treesitter/nvim-treesitter-context"] = {
+    config = function()
+      require "custom.plugins.configs.tscontext"
+    end,
+  },
 
-    ["NvChad/ui"] = {
-		config = function()
-			require("custom.ui").setup()
-		end,
-	},
+  ["NvChad/ui"] = {
+    config = function()
+      require("custom.ui").setup()
+    end,
+  },
 
-    -- Github copilot
-    ["github/copilot.vim"] = {
-        config = function ()
-            require "custom.plugins.configs.copilot"
-        end,
-    },
+  -- Github copilot
+  ["github/copilot.vim"] = {
+    config = function()
+      require "custom.plugins.configs.copilot"
+    end,
+  },
 
-    -- Startuptime
-    ["dstein64/vim-startuptime"] = {},
---------------------------------------------------------------------------------------
-    -- Overriding configs
-    ["nvim-treesitter/nvim-treesitter"] = {
-        override_options = override.treesitter,
-    },
-    ["williamboman/mason.nvim"] = {
-        override_options = override.mason,
-    },
-    ["kyazdani42/nvim-tree.lua"] = {
-        override_options = override.ntree,
-    },
-    ["NvChad/nvterm"] = {
-        override_options = override.nvterm,
-    },
---------------------------------------------------------------------------------------
-    -- Removing plugins
-    ["numToStr/Comment.nvim"] = false,
-    ["folke/which-key.nvim"] = false,
+  -- Startuptime
+  ["dstein64/vim-startuptime"] = {},
+
+  ["jose-elias-alvarez/null-ls.nvim"] = {
+    after = "nvim-lspconfig",
+    config = function()
+      require "custom.plugins.configs.null-ls"
+    end,
+  },
+
+  ["Dereklee0312/base46"] = {
+    branch = "dark_horizon",
+    config = function()
+      local ok, base46 = pcall(require, "base46")
+
+      if ok then
+        base46.load_theme()
+      end
+    end,
+  },
+  --------------------------------------------------------------------------------------
+  -- Overriding configs
+  ["nvim-treesitter/nvim-treesitter"] = {
+    override_options = override.treesitter,
+  },
+  ["williamboman/mason.nvim"] = {
+    override_options = override.mason,
+  },
+  ["kyazdani42/nvim-tree.lua"] = {
+    override_options = override.ntree,
+  },
+  ["NvChad/nvterm"] = {
+    override_options = override.nvterm,
+  },
+  --------------------------------------------------------------------------------------
+  -- Removing plugins
+  ["numToStr/Comment.nvim"] = false,
+  ["folke/which-key.nvim"] = false,
+  ["NvChad/base46"] = false,
 }

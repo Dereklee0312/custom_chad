0a5538deca89bdea281bd4143afe9c5db7e6b6ef
derek_lee <20820343@student.curtin.edu.au>
2022-10-18 16:23:57 +0400

    Changed transparency

--- chadrc.lua
+++ chadrc.lua
@@ -7,10 +7,10 @@ local highlights = require "custom.ui.highlights"
 -- example of changing theme:
 
 M.ui = {
-  theme = "ayu-dark",
+  theme = "ayu-light",
   theme_toggle = { "solarized_dark", "dark_horizon" },
   -- theme_toggle = { "ayu-light", "catppuccin_latte" },
-  transparency = "true",
+  -- transparency = "true",
   hl_override = highlights.override,
   hl_add = highlights.add,
 }

1487c7cab100f9221cf5bfb0d2d61fa5dd3e4a3c
derek_lee <20820343@student.curtin.edu.au>
2022-10-29 01:11:08 +0400

    fix: rearranged init.lua for better structure

--- init.lua
+++ init.lua
@@ -5,25 +5,8 @@
 local opt = vim.opt
 local autocmd = vim.api.nvim_create_autocmd
 
+-- Toggle cmd height when writing a command
 CMDHEIGHT = 0
-
-opt.undofile = false
-
-opt.expandtab = true
-opt.shiftwidth = 4
-opt.smartindent = true
-opt.autoindent = true
-opt.smarttab = true
-opt.tabstop = 4
-opt.softtabstop = 4
-
-opt.relativenumber = true
-
-opt.wrap = false -- No wrap lines
-opt.backup = false
-opt.scrolloff = 10
-opt.sidescrolloff = 20
-opt.clipboard = ""
 
 opt.cmdheight = 0
 
@@ -39,4 +22,31 @@ autocmd({ "RecordingEnter", "RecordingLeave", "CmdlineEnter", "CmdlineLeave" }, 
   end,
 })
 
+-- File stuff
+opt.undofile = false
 vim.cmd "noswapfile"
+
+-- Searching stuff
+opt.iskeyword:append "-" -- consider string-string as whole word
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- Tab stuff
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.autoindent = true
+opt.smarttab = true
+opt.tabstop = 4
+opt.softtabstop = 4
+
+-- Line numbers
+opt.relativenumber = true
+opt.signcolumn = "yes"
+
+-- Buffer stuff
+opt.wrap = false -- No wrap lines
+opt.backup = false
+opt.scrolloff = 10
+opt.sidescrolloff = 20
+opt.clipboard = ""

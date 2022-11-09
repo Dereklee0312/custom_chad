b1145b88efc70b669f5e7b53f1eae89f780bf2be
derek_lee <20820343@student.curtin.edu.au>
2022-11-09 13:03:03 +0400

    added new file for all autocommands

---
+++ autocmd.lua
@@ -0,0 +1,27 @@
+-- File containing all autocommands
+
+local ag = vim.api.nvim_create_augroup
+local au = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
+local opt = vim.opt
+
+autocmd({ "RecordingEnter", "RecordingLeave", "CmdlineEnter", "CmdlineLeave" }, {
+  callback = function()
+    if vim.g.CMDHEIGHT == 0 then
+      opt.cmdheight = 1
+      vim.g.CMDHEIGHT = 1
+    else
+      opt.cmdheight = 0
+      vim.g.CMDHEIGHT = 0
+    end
+  end,
+})
+
+---Highlight yanked text
+au('TextYankPost', {
+  group = ag('yank_highlight', {}),
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup='IncSearch', timeout=300 }
+  end,
+})

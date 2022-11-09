e8fe3a64b096a86f8d428c798732a12bdae75475
derek_lee <20820343@student.curtin.edu.au>
2022-11-09 13:02:54 +0400

    Relocated themes.lua

---
+++ ui/themes.lua
@@ -0,0 +1,18 @@
+local M
+
+M = {
+  tokyonight = {
+    base_30 = {
+      black2 = "NONE",
+      statusline_bg = "NONE",
+    },
+  },
+  solarized_dark = {
+    base_30 = {
+      black2 = "NONE",
+      statusline_bg = "NONE",
+    },
+  },
+}
+
+return M

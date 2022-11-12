4ea419fbe5d6f1efa14e2e16d338ea8f3ce9e4e8
derek_lee <20820343@student.curtin.edu.au>
2022-11-12 18:57:30 +0400

    feat: Added autocmd for formatting on save

--- plugins/configs/null-ls.lua
+++ plugins/configs/null-ls.lua
@@ -5,6 +5,7 @@ if not present then
 end
 
 local b = null_ls.builtins
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local sources = {
 
@@ -30,4 +31,17 @@ local sources = {
 null_ls.setup {
   debug = true,
   sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          vim.lsp.buf.formatting_sync()
+        end,
+      })
+    end
+  end,
 }

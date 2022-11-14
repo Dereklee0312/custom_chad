482454a888b003a0512a664941882fa4237e9c52
derek_lee <20820343@student.curtin.edu.au>
2022-11-14 13:44:30 +0400

    misc

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 -- load your options globals, autocmds here or anything .__.
 -- you can even override default options here (core/options.lua)
 
-require("custom.autocmd")
+require "custom.autocmd"
 
 local opt = vim.opt
 

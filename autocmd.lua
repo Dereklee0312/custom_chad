99b250b85ef2282a7aafdf8fa006af0cca14e725
derek_lee <20820343@student.curtin.edu.au>
2022-11-23 03:35:20 +0400

    feat: cursor now blinks

--- autocmd.lua
+++ autocmd.lua
@@ -25,3 +25,22 @@ au("TextYankPost", {
     vim.highlight.on_yank { higroup = "IncSearch", timeout = 300 }
   end,
 })
+
+-- Automatically deletes all trailing whitespace and newlines at end of file on save. & reset cursor position
+vim.api.nvim_exec(
+  [[
+autocmd BufWritePre * let currPos = getpos(".")
+autocmd BufWritePre * %s/\s\+$//e
+autocmd BufWritePre * %s/\n\+\%$//e
+autocmd BufWritePre *.[ch] %s/\%$/\r/e
+autocmd BufWritePre * cal cursor(currPos[1], currPos[2])
+]],
+  false
+)
+
+-- Blinking Cursor
+vim.cmd [[
+		:set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50
+		  \,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor
+		  \,sm:block-blinkwait175-blinkoff150-blinkon175
+]]
